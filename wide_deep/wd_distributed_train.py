5daa72e39ab4c29d8840754bf5cc5e641fd42fbe
Monu <prankum2100@berkeley.edu>
2018-04-06 16:40:34 -0700

    log every step

--- wide_deep/wd_distributed_train.py
+++ wide_deep/wd_distributed_train.py
@@ -227,7 +227,7 @@ def run(cluster_spec):
                                         categorical_columns,
                                         continuous_columns)
 
-  config = tf.estimator.RunConfig(save_checkpoints_steps=None, save_checkpoints_secs=None)
+  config = tf.estimator.RunConfig(save_checkpoints_steps=None, save_checkpoints_secs=None, log_step_count_steps=1)
 
   estimator = tf.estimator.DNNLinearCombinedClassifier(
       linear_feature_columns=wide_columns,
