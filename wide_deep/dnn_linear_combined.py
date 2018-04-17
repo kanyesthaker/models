c6107724194d46daa45a70c143fed74ea3594b0b
Monu <prankum2100@berkeley.edu>
2018-04-17 14:14:10 -0700

    trying

--- wide_deep/dnn_linear_combined.py
+++ wide_deep/dnn_linear_combined.py
@@ -36,7 +36,6 @@ from tensorflow.python.ops import partitioned_variables
 from tensorflow.python.ops import state_ops
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.summary import summary
-from tensorflow.python.training import sync_replicas_optimizer
 from tensorflow.python.training import training_util
 
 # The default learning rates are a historical artifact of the initial
@@ -44,46 +43,28 @@ from tensorflow.python.training import training_util
 _DNN_LEARNING_RATE = 0.001
 _LINEAR_LEARNING_RATE = 0.005
 
-
-class CombinedOptimizer(tf.train.Optimizer):
-  def __init__(self):
-    print('yo')
-
-def _check_no_sync_replicas_optimizer(optimizer):
-  if isinstance(optimizer, sync_replicas_optimizer.SyncReplicasOptimizer):
-    raise ValueError(
-        'SyncReplicasOptimizer does not support multi optimizers case. '
-        'Therefore, it is not supported in DNNLinearCombined model. '
-        'If you want to use this optimizer, please use either DNN or Linear '
-        'model.')
-
-
 def _linear_learning_rate(num_linear_feature_columns):
-  """Returns the default learning rate of the linear model.
-
-  The calculation is a historical artifact of this initial implementation, but
-  has proven a reasonable choice.
-
-  Args:
-    num_linear_feature_columns: The number of feature columns of the linear
-      model.
-
-  Returns:
-    A float.
-  """
   default_learning_rate = 1. / math.sqrt(num_linear_feature_columns)
   return min(_LINEAR_LEARNING_RATE, default_learning_rate)
 
+class CombinedOptimizer(tf.train.Optimizer):
+  def __init__(self, linear_feature_columns):
+    self.dnn_optimizer = optimizers.get_optimizer_instance('Adagrad', learning_rate=_DNN_LEARNING_RATE)
+    self.linear_optimizer = optimizers.get_optimizer_instance('Ftrl', learning_rate=_linear_learning_rate(len(linear_feature_columns)))
 
-def _add_layer_summary(value, tag):
-  summary.scalar('%s/fraction_of_zero_values' % tag, nn.zero_fraction(value))
-  summary.histogram('%s/activation' % tag, value)
-
+  def minimize(self, loss, ):
+    train_ops = []
+    dnn_ops = self.dnn_optimizer.minimize(loss,var_list=ops.get_collection(ops.GraphKeys.TRAINABLE_VARIABLES, scope='dnn'))
+    linear_ops = self.linear_optimizer.minimize(loss,var_list=ops.get_collection(ops.GraphKeys.TRAINABLE_VARIABLES, scope='linear'))
+    train_ops.append(dnn_ops)
+    train_ops.append(linear_ops)
+    return train_ops
+########################################################################
 
 def _dnn_linear_combined_model_fn(
     features, labels, mode, head,
-    linear_feature_columns=None, linear_optimizer='Ftrl',
-    dnn_feature_columns=None, dnn_optimizer='Adagrad', dnn_hidden_units=None,
+    linear_feature_columns=None,
+    dnn_feature_columns=None, dnn_hidden_units=None,
     dnn_activation_fn=nn.relu, dnn_dropout=None,
     input_layer_partitioner=None, config=None):
 
@@ -99,81 +80,40 @@ def _dnn_linear_combined_model_fn(
           max_partitions=num_ps_replicas,
           min_slice_size=64 << 20))
 
-  # Build DNN Logits.
-  dnn_parent_scope = 'dnn'
+  combined_optimizer = CombinedOptimizer(linear_feature_columns)
+  sync_optimizer = tf.train.SyncReplicasOptimizer(combined_optimizer)
 
-  if not dnn_feature_columns:
-    dnn_logits = None
-  else:
-    dnn_optimizer = optimizers.get_optimizer_instance(
-        dnn_optimizer, learning_rate=_DNN_LEARNING_RATE)
-    _check_no_sync_replicas_optimizer(dnn_optimizer)
-    if not dnn_hidden_units:
-      raise ValueError(
-          'dnn_hidden_units must be defined when dnn_feature_columns is '
-          'specified.')
-    dnn_partitioner = (
-        partitioned_variables.min_max_variable_partitioner(
-            max_partitions=num_ps_replicas))
-    with variable_scope.variable_scope(
-        dnn_parent_scope,
-        values=tuple(six.itervalues(features)),
-        partitioner=dnn_partitioner):
+  dnn_partitioner = (
+      partitioned_variables.min_max_variable_partitioner(
+          max_partitions=num_ps_replicas))
+  with variable_scope.variable_scope(
+      dnn_parent_scope,
+      values=tuple(six.itervalues(features)),
+      partitioner=dnn_partitioner):
 
-      dnn_logit_fn = dnn._dnn_logit_fn_builder(  # pylint: disable=protected-access
-          units=head.logits_dimension,
-          hidden_units=dnn_hidden_units,
-          feature_columns=dnn_feature_columns,
-          activation_fn=dnn_activation_fn,
-          dropout=dnn_dropout,
-          input_layer_partitioner=input_layer_partitioner)
-      dnn_logits = dnn_logit_fn(features=features, mode=mode)
+    dnn_logit_fn = dnn._dnn_logit_fn_builder(
+        units=head.logits_dimension,
+        hidden_units=dnn_hidden_units,
+        feature_columns=dnn_feature_columns,
+        activation_fn=dnn_activation_fn,
+        dropout=dnn_dropout,
+        input_layer_partitioner=input_layer_partitioner)
+    dnn_logits = dnn_logit_fn(features=features, mode=mode)
 
-  linear_parent_scope = 'linear'
+  with variable_scope.variable_scope(
+      linear_parent_scope,
+      values=tuple(six.itervalues(features)),
+      partitioner=input_layer_partitioner) as scope:
+    logit_fn = linear._linear_logit_fn_builder(
+        units=head.logits_dimension,
+        feature_columns=linear_feature_columns)
+    linear_logits = logit_fn(features=features)
 
-  if not linear_feature_columns:
-    linear_logits = None
-  else:
-    linear_optimizer = optimizers.get_optimizer_instance(
-        linear_optimizer,
-        learning_rate=_linear_learning_rate(len(linear_feature_columns)))
-    _check_no_sync_replicas_optimizer(linear_optimizer)
-    with variable_scope.variable_scope(
-        linear_parent_scope,
-        values=tuple(six.itervalues(features)),
-        partitioner=input_layer_partitioner) as scope:
-      logit_fn = linear._linear_logit_fn_builder(  # pylint: disable=protected-access
-          units=head.logits_dimension,
-          feature_columns=linear_feature_columns)
-      linear_logits = logit_fn(features=features)
-      _add_layer_summary(linear_logits, scope.name)
-
-  # Combine logits and build full model.
-  if dnn_logits is not None and linear_logits is not None:
-    logits = dnn_logits + linear_logits
-  elif dnn_logits is not None:
-    logits = dnn_logits
-  else:
-    logits = linear_logits
+  logits = dnn_logits + linear_logits
 
   def _train_op_fn(loss):
     """Returns the op to optimize the loss."""
-    train_ops = []
-    global_step = training_util.get_global_step()
-    if dnn_logits is not None:
-      train_ops.append(
-          dnn_optimizer.minimize(
-              loss,
-              var_list=ops.get_collection(
-                  ops.GraphKeys.TRAINABLE_VARIABLES,
-                  scope=dnn_parent_scope)))
-    if linear_logits is not None:
-      train_ops.append(
-          linear_optimizer.minimize(
-              loss,
-              var_list=ops.get_collection(
-                  ops.GraphKeys.TRAINABLE_VARIABLES,
-                  scope=linear_parent_scope)))
+    train_ops = sync_optimizer.minimize(loss)
 
     train_op = control_flow_ops.group(*train_ops)
     with ops.control_dependencies([train_op]):
@@ -192,9 +132,7 @@ class DNNLinearCombinedClassifier(estimator.Estimator):
   def __init__(self,
                model_dir=None,
                linear_feature_columns=None,
-               linear_optimizer='Ftrl',
                dnn_feature_columns=None,
-               dnn_optimizer='Adagrad',
                dnn_hidden_units=None,
                dnn_activation_fn=nn.relu,
                dnn_dropout=None,
@@ -227,9 +165,7 @@ class DNNLinearCombinedClassifier(estimator.Estimator):
           mode=mode,
           head=head,
           linear_feature_columns=linear_feature_columns,
-          linear_optimizer=linear_optimizer,
           dnn_feature_columns=dnn_feature_columns,
-          dnn_optimizer=dnn_optimizer,
           dnn_hidden_units=dnn_hidden_units,
           dnn_activation_fn=dnn_activation_fn,
           dnn_dropout=dnn_dropout,
